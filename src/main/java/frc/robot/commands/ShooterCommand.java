e8f7c1be984af7084dbe4d87db8db87edaa670f2
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2024-02-03 10:12:35 -0500

    Small fixes to shooter

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -3,56 +3,52 @@ package frc.robot.commands;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** An example command that uses an example subsystem. */
+/** An shoot command that uses an shooter subsystem. */
 public class ShooterCommand extends Command {
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
- 
-    private final ShooterSubsystem m_subsystem;
-    /*private enum ShooterStatus {MotorRunning, MotorStopped};
-    private static ShooterStatus m_status;*/
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
-    public static enum CommandType {StartMotor, Shoot};
+    private final ShooterSubsystem m_subsystem;
+
+    public static enum CommandType {
+        StartMotor, Shoot
+    };
+
     private final CommandType m_command;
 
     public ShooterCommand(ShooterSubsystem subsystem, CommandType command) {
         m_subsystem = subsystem;
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(subsystem);
-
         m_command = command;
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+    }
 
-     // Called every time the scheduler runs while the command is scheduled.
+    // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         if (m_command == CommandType.StartMotor) {
-            // if start button, run
-            if (!ShooterSubsystem.IsShooterRunning()) {
+            if (!ShooterSubsystem.isShooterRunning()) {
                 m_subsystem.runShooter();
-            }
-            else {
+            } else {
                 m_subsystem.stopShooter();
             }
-            // if end button stop
-        }
-        else if (m_command == CommandType.Shoot) {
-            // shoot thing
+        } else if (m_command == CommandType.Shoot) {
+            // Finish making shootCycle command in shooter subsystem
         }
 
     }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -17,86 +17,79 @@ import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.networktables.GenericEntry;
 
-
-
 /* keeping these incase we need them
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 */
 
-public class ShooterSubsystem extends SubsystemBase{
-    //private double m_motorpower = 0.0; <-- not sure we even need this var...
+public class ShooterSubsystem extends SubsystemBase {
+  // private double m_motorpower = 0.0; <-- not sure we even need this var...
 
-    private final ShuffleboardTab m_shuffleboardtab = Shuffleboard.getTab("Shooter");
+  private final ShuffleboardTab m_shuffleboardtab = Shuffleboard.getTab("Shooter");
 
-    private final GenericEntry m_speedentry;
-    //private final NetworkTable m_tab = NetworkTableInstance.getDefault().getTable("Shooter");
-    public final CANSparkMax m_leftMotor;
-    public final CANSparkMax m_rightMotor;
-    private
-     static boolean ShooterRunning = false;
-    
-    public ShooterSubsystem(double defaultpower) {
-        m_speedentry = m_shuffleboardtab.add("Motor Power",defaultpower).withWidget(BuiltInWidgets.kNumberSlider)
-                                                                              .withProperties(Map.of("min",-1.0,"max",1.0))
-                                                                              .getEntry();
+  private final GenericEntry m_speedentry;
+  public final CANSparkMax m_leftMotor;
+  public final CANSparkMax m_rightMotor;
+  private static boolean shooterRunning = false;
 
-        m_shuffleboardtab.addBoolean("Motor Spinning", () -> ShooterSubsystem.IsShooterRunning());
+  public ShooterSubsystem(double defaultpower) {
+    // Slider to control power from shuffleboard
+    m_speedentry = m_shuffleboardtab.add("Motor Power", defaultpower).withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min", -1.0, "max", 1.0))
+        .getEntry();
 
-        m_leftMotor = new CANSparkMax(ShooterConstants.kRightDeviceId, MotorType.kBrushless); // Assuming brushless? 
-        m_rightMotor = new CANSparkMax(ShooterConstants.kLeftDeviceId, MotorType.kBrushless);
+    m_shuffleboardtab.addBoolean("Motor Spinning", () -> ShooterSubsystem.isShooterRunning());
+
+    m_leftMotor = new CANSparkMax(ShooterConstants.kRightDeviceId, MotorType.kBrushed); // Will probably be changed to brushless later
+    m_rightMotor = new CANSparkMax(ShooterConstants.kLeftDeviceId, MotorType.kBrushed);
+  }
+
+  private static double clampPower(double power) {
+    return MathUtil.clamp(power, -1.0, 1.0);
+  }
+
+  public double getPower() {
+    return m_speedentry.getDouble(0.0);
+  }
+
+  public void setPower(double fracpower) {
+    m_speedentry.setDouble(fracpower);
+  }
+
+  public void runShooter() {
+    m_leftMotor.set(clampPower(getPower()));
+    m_rightMotor.set(clampPower(getPower()));
+    shooterRunning = true; // This was set to false, probably an accident
+  }
+
+  public void stopShooter() {
+    m_leftMotor.stopMotor();
+    m_rightMotor.stopMotor();
+    shooterRunning = false;
+  }
+
+  public void shootCycle() {
+    if (!shooterRunning) {
+      System.console().printf("WARNING: ShooterSubsystem tried a shoot cycle when the motors were not running!");
+      return;
     }
+    /* code to shoot a single ring goes here */
+  }
 
-    private static double clampPower(double power) {
-        return MathUtil.clamp(power, -1.0, 1.0);
-    }
-    // power of moter is in the range from -1.0 to 1.0
+  public static boolean isShooterRunning() {
+    return shooterRunning;
+  }
 
-    public double GetPower() {
-        return m_speedentry.getDouble(0.0);
-    }
-
-    public void SetPower(double fracpower) {
-        m_speedentry.setDouble(fracpower);
-        //m_motorpower = fracpower;
-    }
-
-    public void runShooter() {
-        m_leftMotor.set(clampPower(m_speedentry.getDouble(0.0)));
-        m_rightMotor.set(clampPower(m_speedentry.getDouble(0.0)));
-
-        ShooterRunning = false;
-    }
-
-    public void stopShooter() {
-        m_leftMotor.stopMotor();
-        m_rightMotor.stopMotor();
-
-        ShooterRunning = false;
-    }
-
-    public void ShootCycle() {
-        if (!ShooterRunning) {
-            System.console().printf("WARNING: ShooterSubsystem tried a shoot cycle when the motors were not running!");
-            return;
-        }
-        /*code to shoot a single ring goes here*/
-    }
-
-    public static boolean IsShooterRunning() {
-        return ShooterRunning;
-    }
-
-    @Override
-    public void periodic() {
+  @Override
+  public void periodic() {
     // This method will be called once per scheduler run
-    
-    }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
-    }
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 
 }
