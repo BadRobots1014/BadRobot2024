3f8dbb7ae2fd61b5a4e967d5d027a19ccf269acb
Fyrefyte <leowilliamcanales@gmail.com>
2024-02-03 11:38:21 -0500

    Optimization fixed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 
-// import com.revrobotics.CANSparkMax.IdleMode;
-
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
