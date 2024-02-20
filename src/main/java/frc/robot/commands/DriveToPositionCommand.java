7bab0f92802338e9434effbf011ffe461d5580e2
Fyrefyte <leowilliamcanales@gmail.com>
2024-02-19 20:23:14 -0500

    More progress

--- src/main/java/frc/robot/commands/DriveToPositionCommand.java
+++ src/main/java/frc/robot/commands/DriveToPositionCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -11,80 +12,53 @@ import java.util.function.Supplier;
 
 public class DriveToPositionCommand extends Command {
 
-  public final SwerveSubsystem swerveSubsystem;
-  public final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
-  public final Supplier<Boolean> fieldOrientedFunction, fastModeFunction;
+  public final SwerveSubsystem swerve;
+  public final SwerveDriveCommand driveCommand;
+  public final Supplier<Double> goalX, goalY, goalAngle, xSpdFunction, ySpdFunction, turningSpdFunction;
+  public final Supplier<Boolean> fieldOrientedFunction, fastModeFunction, reset;
   public final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
 
   public DriveToPositionCommand(
     SwerveSubsystem subsystem,
-    Supplier<Double> xSupplier,
-    Supplier<Double> ySupplier,
-    Supplier<Double> turnSupplier,
+    Supplier<Double> goalX,
+    Supplier<Double> goalY,
+    Supplier<Double> goalAngle,
     Supplier<Boolean> fieldOriented,
-    Supplier<Boolean> fastMode
+    Supplier<Boolean> fastMode,
+    Supplier<Boolean> resetOnStart
   ) {
-    swerveSubsystem = subsystem;
-    xSpdFunction = xSupplier;
-    ySpdFunction = ySupplier;
-    turningSpdFunction = turnSupplier;
+    swerve = subsystem;
+    this.goalX = goalX;
+    this.goalY = goalY;
+    this.goalAngle = goalAngle;
+    this.xSpdFunction = goalX;
+    this.ySpdFunction = goalY;
+    this.turningSpdFunction = goalAngle;
     fieldOrientedFunction = fieldOriented;
     fastModeFunction = fastMode;
     xLimiter = new SlewRateLimiter(DriveConstants.kXSlewRateLimit);
     yLimiter = new SlewRateLimiter(DriveConstants.kYSlewRateLimit);
     turningLimiter = new SlewRateLimiter(DriveConstants.kTurnSlewRateLimit);
-    addRequirements(swerveSubsystem);
+    reset = resetOnStart;
+    driveCommand = new SwerveDriveCommand(subsystem, xSpdFunction, ySpdFunction, turningSpdFunction, fieldOriented, fastMode);
+    addRequirements(swerve);
+  }
+
+  @Override
+  public void initialize() {
+    if (reset.get()) {
+      swerve.resetPose(new Pose2d());
+    }
   }
 
   @Override
   public void execute() {
-    // Get inputs
-    double xSpeed = xSpdFunction.get();
-    double ySpeed = ySpdFunction.get();
-    double turningSpeed = turningSpdFunction.get();
-    boolean fastMode = fastModeFunction.get();
-
-    // Death
-    xSpeed = Math.abs(xSpeed) > OIConstants.kDriveDeadband ? xSpeed : 0;
-    ySpeed = Math.abs(ySpeed) > OIConstants.kDriveDeadband ? ySpeed : 0;
-    turningSpeed =
-      Math.abs(turningSpeed) > OIConstants.kDriveDeadband ? turningSpeed : 0;
-
-    // Slew soup
-    double maxDriveSpeed = fastMode ? DriveConstants.kFastTeleMaxMetersPerSec : DriveConstants.kTeleMaxMetersPerSec;
-    double maxTurnSpeed = fastMode ? DriveConstants.kFastTeleMaxRadiansPerSec : DriveConstants.kTeleMaxRadiansPerSec;
-    xSpeed = xLimiter.calculate(xSpeed) * maxDriveSpeed;
-    ySpeed = yLimiter.calculate(ySpeed) * maxDriveSpeed;
-    turningSpeed =
-      turningLimiter.calculate(turningSpeed) *
-      maxTurnSpeed;
-
-    // I am speed
-    ChassisSpeeds chassisSpeeds;
-    if (fieldOrientedFunction.get()) {
-      // Field oriented
-      chassisSpeeds =
-        ChassisSpeeds.fromFieldRelativeSpeeds(
-          xSpeed,
-          ySpeed,
-          turningSpeed,
-          swerveSubsystem.getRotation2d()
-        );
-    } else {
-      chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
-    }
-
-    // Divide and conker
-    SwerveModuleState[] moduleStates =
-      DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-
-    // Actually do the thing
-    swerveSubsystem.setModuleStates(moduleStates);
+    
   }
 
   @Override
   public void end(boolean interrupted) {
-    swerveSubsystem.stopModules();
+    swerve.stopModules();
   }
 
   @Override
