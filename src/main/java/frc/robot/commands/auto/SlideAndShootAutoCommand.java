2fdb61fcc58b6671d04c4f6d47fa85ae6d9b834e
Fyrefyte <leowilliamcanales@gmail.com>
2024-03-06 08:45:28 -0500

    Fixy

--- src/main/java/frc/robot/commands/auto/SlideAndShootAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/SlideAndShootAutoCommand.java
@@ -22,12 +22,12 @@ public class SlideAndShootAutoCommand extends SequentialCommandGroup {
     super(
         new WaitCommand(4),
       new SetPoseCommand(swerve, startingOffset).withTimeout(0),
-      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? -.3 : .3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? -.3 : .3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyBoolean(false), supplyDouble(0))
       .withTimeout(1),
       new ShootCommand(shoot),
-      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? .3 : -.3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? .3 : -.3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyBoolean(false), supplyDouble(0))
       .withTimeout(1),
-      new SwerveDriveCommand(swerve, supplyDouble(0), supplyDouble(-.3), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+      new SwerveDriveCommand(swerve, supplyDouble(0), supplyDouble(-.3), supplyDouble(0), true, supplyBoolean(true), supplyBoolean(false), supplyDouble(0))
     );
   }
 
