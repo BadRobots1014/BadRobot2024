ecc9e2e74172fbd064515481a5e7ef0127249d1a
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2024-03-06 08:41:23 -0500

    asdfaf

---
+++ src/main/java/frc/robot/commands/auto/SlideAndShootAutoCommand.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.auto;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.SetPoseCommand;
+import frc.robot.commands.ShootCommand;
+import frc.robot.commands.SwerveDriveCommand;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SlideAndShootAutoCommand extends SequentialCommandGroup {
+  public SlideAndShootAutoCommand(ShooterSubsystem shoot, SwerveSubsystem swerve, Pose2d startingOffset, int direction) {
+    super(
+        new WaitCommand(4),
+      new SetPoseCommand(swerve, startingOffset).withTimeout(0),
+      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? -.3 : .3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+      .withTimeout(1),
+      new ShootCommand(shoot),
+      new SwerveDriveCommand(swerve, supplyDouble(direction == 0 ? .3 : -.3), supplyDouble(0), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+      .withTimeout(1),
+      new SwerveDriveCommand(swerve, supplyDouble(0), supplyDouble(-.3), supplyDouble(0), true, supplyBoolean(true), supplyDouble(0))
+    );
+  }
+
+  private static Supplier<Double> supplyDouble(double d) {return new Supplier<Double>() {@Override public Double get() {return d;}};}
+  private static Supplier<Boolean> supplyBoolean(boolean b) {return new Supplier<Boolean>() {@Override public Boolean get() {return b;}};}
+}
