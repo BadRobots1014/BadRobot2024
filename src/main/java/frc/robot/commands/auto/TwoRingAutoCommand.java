e2726cbdf9109e11d7fc03868e620d50e2fd262d
Fyrefyte <leowilliamcanales@gmail.com>
2024-04-04 10:08:49 -0400

    xtras

--- src/main/java/frc/robot/commands/auto/TwoRingAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/TwoRingAutoCommand.java
@@ -8,7 +8,6 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.GroundIntakeCommand;
