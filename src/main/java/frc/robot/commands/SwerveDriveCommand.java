fb1917f1a7cee821d2c9d4704f548d3a4aeb1fcd
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2024-04-17 12:02:55 -0400

    Removed sussy logic

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -71,53 +71,7 @@ public class SwerveDriveCommand extends Command {
       xSpeed = pov.get() == 90 ? -DriveConstants.kNudgeSpeed : (pov.get() == 270 ? DriveConstants.kNudgeSpeed : 0);
       ySpeed = pov.get() == 0 ? DriveConstants.kNudgeSpeed : (pov.get() == 180 ? -DriveConstants.kNudgeSpeed : 0);
     }
-    double presetTurnSpeedFactor = 0;
-    double targetTheta = 0;//set preset angle target
-    isPresetTurnActive = (Math.abs(auxLTrig.get()) > OIConstants.kDriveDeadband || Math.abs(auxRTrig.get()) > OIConstants.kDriveDeadband);
-
-    if(Math.abs(auxLTrig.get()) > OIConstants.kDriveDeadband){ //yes I know this is redundent
-      targetTheta = DriveConstants.kSpeakerTheta;
-      presetTurnSpeedFactor = auxLTrig.get();
-    }else if(Math.abs(auxRTrig.get()) > OIConstants.kDriveDeadband){//Left will take precident over right
-      targetTheta = DriveConstants.kSourceTheta;
-      presetTurnSpeedFactor = auxRTrig.get();
-    }
-
-    presetTurnSpeedFactor = MathUtil.clamp(presetTurnSpeedFactor, -1.0, 1.0);
-
-    //autoturny stuffs
-    double currentHeading = swerveSubsystem.getHeading();
-    //double currentRawHeading = swerveSubsystem.getHeading();
-    double theta,speed;
-    
-    
-    theta = targetTheta - currentHeading;
-    if(targetTheta < 0 && currentHeading >= 0 && currentHeading < 180 + targetTheta){
-      if(currentHeading < 10){
-        theta -= .1; //keep robot moving over the boundery
-      }
-      if(currentHeading < 0 || currentHeading > 180 + targetTheta){
-        theta = (targetTheta + 360) - currentHeading; //robot move in correct direction once over boundery
-      }
-    }else if(targetTheta > 0 && currentHeading <= 0 && currentHeading > 180 + targetTheta){
-      if(currentHeading > 350){
-        theta += .1; //keep robot moving over the boundery
-      }
-      if(currentHeading > 359 || currentHeading < targetTheta){
-        theta = (360 - targetTheta) - currentHeading;//robot move in correct direction once over boundery
-      }
-    }
-    speed = theta / 45;
-
-    System.out.println("DeltaTheta:" + theta);
-    System.out.println("TargetTheta:" + targetTheta);
-    System.out.println("Current Heading:" + currentHeading);
-    //System.out.println("Current Raw Heading:" + currentRawHeading);
-
-    if(isPresetTurnActive){
-      //if a trigger is held down override turningspeed
-      turningSpeed = MathUtil.clamp(speed * presetTurnSpeedFactor, -DriveConstants.kTurnThetaMaxSpeed, DriveConstants.kTurnThetaMaxSpeed);
-    }
+   
 
 
     // Death
